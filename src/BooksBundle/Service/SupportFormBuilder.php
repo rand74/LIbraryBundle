92e80a122eb39e1cd6bd38ebcdf20069bedf5672
Ducruet david <david.ducruet@orange.fr>
2017-08-23 11:17:57 +0200

    wip refactoring addaction to service

---
+++ src/BooksBundle/Service/SupportFormBuilder.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: david
+ * Date: 23/08/17
+ * Time: 11:00
+ */
+
+namespace BooksBundle\Service;
+
+
+use BooksBundle\Enum\SupportTypeEnum;
+use BooksBundle\Form\Type\BookType;
+use BooksBundle\Form\Type\ComicStripType;
+use BooksBundle\Form\Type\EbookType;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormFactory;
+use Symfony\Component\HttpFoundation\Request;
+
+class SupportFormBuilder
+{
+    protected $em;
+    protected $twig;
+    protected $form;
+    protected $request;
+    protected $formFactory;
+
+    public function __construct(EntityManager $em, \Twig_Environment $twig, Form $form, Request $request, FormFactory $formFactory)
+    {
+        $this->em           = $em;
+        $this->twig         = $twig;
+        $this->form         = $form;
+        $this->request      = $request;
+        $this->formFactory  = $formFactory;
+    }
+
+    public function addFormSupport($page)
+    {
+        $typeName = SupportTypeEnum::$typeName;
+        $types = [
+            $typeName[SupportTypeEnum::TYPE_BOOK] => BookType::class,
+            $typeName[SupportTypeEnum::TYPE_EBOOK] => EbookType::class,
+            $typeName[SupportTypeEnum::TYPE_COMIC_STRIP] => ComicStripType::class
+        ];
+
+        $forms =[];
+        foreach ($types as $type) {
+            $forms[] = $this->formFactory->createForm($type);
+        }
+
+        if ($this->request->isMethod('POST')) {
+            foreach ($forms as $form) {
+                $form->handleRequest($this->request);
+
+                if (!$form->isSubmitted()) continue;
+
+                if ($form->isValid()) {
+                    $this->em->persist($form);
+                    $this->em->flush();
+                    break;
+                }
+            }
+        }
+
+        $views = [];
+        foreach ($forms as $form) {
+            $views[] = $form->createView();
+        }
+        return $this->twig->render($page, array(
+            'forms' => $views,
+            'types' => $types
+        ));
+    }
+}
